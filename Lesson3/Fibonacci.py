67ad72a273d49e2e8d5f995b4dc956400ee85ac5
Vike Hsu <vikehsu@gmail.com>
2023-10-11 18:53:43 -0400

    Add dynamic programming for Fibonacci.

--- Lesson3/Fibonacci.py
+++ Lesson3/Fibonacci.py
@@ -5,7 +5,7 @@ F(n) = F(n - 1) + F(n - 2), for n > 1.
 dict1 = {'key1':'data1','key2':'data2'}
 print(dict1['key1'])
 
-N = 50
+N = input("Type the fib number you want to calculate")
 
 fib_known = {0:0,1:1}
 
@@ -17,6 +17,19 @@ def fib_new(n):
         fib_known[n] = result
         return result
 
+def fib_dp(n):
+    a = 0
+    b = 1
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    for i in range(2, n):
+        c = a + b
+        a = b
+        b = c
+    return a + b
+
 def fib(n):
     if n == 0:
         return 0
@@ -25,10 +38,9 @@ def fib(n):
     # if n > 1:
     return fib(n-1) + fib(n-2)
 
-
 # test:
+print(fib_dp(N))
+
 print(fib_new(N))
 
-print(fib_new(N+1))
-
-print(fib(N))
+#print(fib(N))
